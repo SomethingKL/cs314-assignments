9485bded0f4a83ae4f04e36783e3e2f074651aac
rbrandt <rossmbrandt@gmail.com>
2018-05-07 23:52:37 -0700

    api refs

--- Pages/API/index.js
+++ Pages/API/index.js
@@ -1,138 +1,44 @@
 // Ross Brandt
 
 // get the data from the users section of jsonplaceholder
-$.get('https://rickandmortyapi.com/api/character/')
- .done((data) => populateUsers(data))
+$.get('https://holidayapi.com/v1/holidays?key=ebaee58e-f958-4970-b7c6-828eb1ea5927&country=US&year=2017&month=05')
+ .done((data) => populateHolidays(data.holidays))
  .fail(() => {});
 
 // for each user get information and display
-let populateUsers = (users) => users.forEach(
-	(user) => $('#users').append(
-		generateUser({
-			id        : user.id,
-			name      : user.name,
-			email     : user.email,
-			company   : user.company.name
+let populateHolidays = (day) => day.forEach(
+	(day) => $('#days').append(
+		generateHoliday({
+			name      : day.name,
+			date      : day.date,
+			observed  : day.observed,
+			bank      : day.public
 		})
 	)
 );
 
-// generate all the user elements within a div
-let generateUser = (user) => {
-	// create user elements
-	let userSection = document.createElement("div");
-	let userName = document.createElement("h4");
-	let userEmail = document.createElement("p");
-	let userCompany = document.createElement("p");
-
-	// populate user info
-	userSection.className = "userSection";
-	userSection.setAttribute('id', `user${user.id}`);
-	userName.innerHTML = user.name;
-	userSection.append(userName);
-	userEmail.innerHTML = user.email;
-	userSection.append(userEmail);
-	userCompany.innerHTML = user.company;
-	userSection.append(userCompany);
-
-	// add button elements
-	let userButtons = document.createElement("div");
-	userButtons.className = "userButtons";
-
-	// todo button
-	let todoButton = document.createElement("button");
-	todoButton.className = "button";
-	todoButton.innerHTML = "Show Todos";
-	todoButton.setAttribute("notLoaded", true);
-	todoButton.onclick = () => {
-		if($(todoButton).attr("notLoaded")){
-			$.get(`https://jsonplaceholder.typicode.com/todos?userId=${user.id}`)
-			 .done((data) => {
-				todoButton.setAttribute("notLoaded", false);
-				$(userButtons).slideToggle();
-				loadTodos(data, user.id);
-				$(userButtons).slideToggle();
-			})
-			.fail(() => {
-				console.log("failed");
-			});
-		} else {
-			$(`#todoList${user.id}`).toggle();
-			$(`#albumList${user.id}`).hide();
-		}
-	};
-	userButtons.append(todoButton);
+let generateHoliday = (info) => {
+	let container = document.createElement("section");
+	container.setAttribute('id', "card");
 	
-	// albums button
-	let albumButton = document.createElement("button");
-	albumButton.className = "button";
-	albumButton.innerHTML = "Show Albums";
-	albumButton.setAttribute("notLoaded", true);
-	albumButton.onclick = () => {
-		if($(albumButton).attr("notLoaded")){
-			$.get(`https://jsonplaceholder.typicode.com/todos?userId=${user.id}`)
-			 .done((data) => {
-				albumButton.setAttribute("notLoaded", false);
-				$(userButtons).slideToggle();
-				loadAlbums(data, user.id);
-				$(userButtons).slideToggle();
-			})
-			.fail(() => {
-				console.log("failed");
-			});
-		} else {
-			$(`#albumList${user.id}`).toggle();
-			$(`#todoList${user.id}`).hide();
-		}
-	};
-	userButtons.append(albumButton);
+	let holiday = document.createElement("section");
+	holiday.setAttribute('id', "day");
 	
-	userSection.append(userButtons);
-
-	$("#users").append(userSection);
-	return user;
-}
-
-// load the todos into a container
-function loadTodos(data, userID) {
-	let todoList = document.createElement("div");
+	let title = document.createElement("h3");
+	title.setAttribute('id', "name");
+	title.innerHTML = info.name;
+	holiday.append(title);
 	
-	todoList.className = "todoList";
-	todoList.id = `todosList${userID}`;
+	let description = document.createElement("p");
+	description.setAttribute('id', "info");
+	description.innerHTML = "Takes place on " +info.date+". ";
+	holiday.append(description);
 	
-	let todosHeader = document.createElement("h5");
-	todosHeader.innerHTML = "Todos:";
-
-	for (let todo of data){
-		let todoItem = document.createElement("div");
-		todoItem.className="todoItem";
-		if (todo.completed === true){
-			todoItem.innerHTML=`<i class="far fa-check-square" style="margin-right:5px;"></i>${todo.title}`;
-		} else {
-			todoItem.innerHTML=`<i class="far fa-square" style="margin-right:5px;"></i>${todo.title}`;
-		}
-
-		todoList.append(todoItem);
-	}
-	$(`#user${userID}`).append(todoList);
-}
-
-// load the albums into a container
-function loadAlbums(data, userID) {
-	let albumList = document.createElement("div");
+	description = document.createElement("p");
+	description.setAttribute('id', "info");
+	description.innerHTML = "Celebrated on "+info.date+".";
+	holiday.append(description);
 	
-	albumList.className = "albumList";
-	albumList.id = `albumList${userID}`;
-	
-	let AlbumHeader = document.createElement("h5");
-	AlbumHeader.innerHTML = "Todos:";
-
-	for (let album of data){
-		let albumItem = document.createElement("div");
-		albumItem.className="albumItem";
-		albumItem.innerHTML=`- ${album.title}`;
-
-		albumList.append(albumItem);
-	}
-	$(`#user${userID}`).append(albumList);
+	container.append(holiday);
+	$("#days").append(container);
 }
